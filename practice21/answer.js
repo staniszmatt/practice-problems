2e362502ffd008b4d4547dcf74d1e83b268c0e4a
staniszmatt <matthewpstaniszewski@gmail.com>
2019-01-02 18:55:52 -0800

    practice21 | Bipolar Loop | Matthew Staniszewski

--- practice21/answer.js
+++ practice21/answer.js
@@ -1,1 +1,8 @@
-answer.js
+var numberArray = [1, 2, 3, 4 ,5, 6];
+
+for(i = 0, j = numberArray.length-1; i <= numberArray.length, j>=0; i++, j--){
+    console.log("i is: ",i);
+    console.log("number Array number i: ",numberArray[i]);
+    console.log("j is: ",j);
+    console.log("number array j: ", numberArray[j]);
+};
