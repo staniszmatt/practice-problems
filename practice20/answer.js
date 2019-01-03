953df498ff1518cd5e85bd109b810897258beb1e
staniszmatt <matthewpstaniszewski@gmail.com>
2019-01-02 18:20:28 -0800

    practice20 | Randomize Array | Matthew Staniszewski

--- practice20/answer.js
+++ practice20/answer.js
@@ -1,1 +1,23 @@
-answer.js
+var arrayNumber = [1, 2, 3, 4 , 5, 6, 8, 9];
+var arrayLetters = ["a", "b", "c", "e", "f", "g"];
+
+
+var randomArray = randomize_list(arrayNumber);
+var randomLetterArray = randomize_list(arrayLetters);
+
+function randomize_list(passArray){
+    var arrayIndex = passArray.length;
+    var tempStorage = 0;
+    var randomIndex = 0; 
+    while (0 !== arrayIndex) {
+        randomIndex = Math.floor(Math.random() * arrayIndex);
+        arrayIndex -= 1; 
+        tempStorage = passArray[arrayIndex];
+        passArray[arrayIndex] = passArray[randomIndex];
+        passArray[randomIndex] = tempStorage;
+    }; 
+    return passArray;
+}; 
+
+console.log("Random Array is: ", randomArray);
+console.log("Random Letters Array: ", randomLetterArray);
